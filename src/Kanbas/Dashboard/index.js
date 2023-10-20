eebe13a1dbe5f3a676be06a111271e50d57175de
lucklyjosh <lucklyjosh@gmail.com>
2023-10-19 21:53:18 -0400

    JavaScript and React.js Assignment 3

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -9,18 +9,17 @@ function Dashboard() {
         <div>
             <h1>Dashboard</h1>
             <hr />
-            <div class="ml-5">
+            <div className="ml-5">
                 <h2>Published Courses ({courses.length})</h2>
             </div>
 
             <div className="main-content p-4">
                 <div className="row">
                     {courses.map(course => (
-                        <div key={course._id} className="col-4">
+                        <div key={course._id} className="col-lg-4 col-md-6 col-sm-12 mb-4">
                             <div className="card h-100 card-spacing">
                                 <div className="card-image">
                                     <BsThreeDotsVertical className="card-icon" style={{ color: 'white' }} />
-
                                 </div>
                                 <div className="card-body">
                                     <h5 className="card-title">{course.name}</h5>
